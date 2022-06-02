05f6f8896e022a25565eecf559665bff0774bedc
kiran <kiran@KIRAN>
2022-06-03 00:21:01 +0530

    day4

---
+++ class1/src/class1/Demo2.java
@@ -0,0 +1,15 @@
+package class1;
+
+public class Demo2 {
+	int x;
+    public static void main (String[]args)	{
+	   Demo2 myobj1 =new Demo2();
+	   Demo2 myobj2=new Demo2();
+	  
+	   System.out.println(myobj1.x);
+	   System.out.println(myobj2.x);
+}
+}
+
+
+
