bf7a5fadf24e1c55674a7976747215a5f11104e6
kiran <kiran@KIRAN>
2022-06-03 10:17:50 +0530

    Using class and object

---
+++ class1/src/class1/Demo1.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class Demo1 {
+    int x;
+    public static void main (String[]args)	{
+	   Demo2 myobj1 =new Demo2();
+	   Demo2 myobj2=new Demo2();
+	      myobj1.x=20;
+	      myobj2.x=47;
+	   System.out.println(myobj1.x);
+	   System.out.println(myobj2.x);
+}
+}
+
+
+
