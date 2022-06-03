e44790f7f7eb40993e3cfbabfa15692ece68286f
kiran <kiran@KIRAN>
2022-06-03 10:31:39 +0530

    class and object examples

---
+++ class1/src/class1/Demo3.java
@@ -0,0 +1,23 @@
+package class1;
+
+public class Demo3 {
+    int x;
+    public static void main (String[]args)	{
+	   Demo3 myobj1 =new Demo3();
+	   Demo3 myobj2=new Demo3();
+	   Demo3 myobj3=new Demo3();
+	   Demo3 myobj4=new Demo3();
+	      myobj1.x=20;
+	      myobj2.x=47;
+	      myobj3.x=54;
+	      myobj4.x=42;
+	   System.out.println(myobj1.x);
+	   System.out.println(myobj2.x);
+	   System.out.println(myobj3.x);
+	   System.out.println(myobj4.x);
+}
+}
+
+
+
+
---
+++ class1/src/class1/Demo4.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class Demo4 {
+    float x;
+    public static void main (String[]args)	{
+	   Demo4 myobj1 =new Demo4();
+	   Demo4 myobj2=new Demo4();
+	     
+	   System.out.println(myobj1.x);
+	   System.out.println(myobj2.x);
+}
+}
+
+
+
+
